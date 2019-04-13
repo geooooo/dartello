27ad8fc3cbb064580b7ef145eee042605da0ed7f
yury kornienko <geo97it@gmail.com>
2019-04-14 02:01:25 +0300

    append config

---
+++ server/lib/src/internal/app_config.dart
@@ -0,0 +1,12 @@
+import 'dart:io' as io;
+
+import 'package:aqueduct/aqueduct.dart';
+
+class AppConfig extends Configuration {
+
+  static const String configPath = 'config.yaml';
+  DatabaseConfiguration database;
+
+  AppConfig() : super.fromFile(io.File(configPath));
+
+}
--- server/lib/src/internal/managed_context.dart
+++ server/lib/src/internal/managed_context.dart
@@ -1,1 +1,18 @@
-Object createManagedContext() => null;
+import 'package:aqueduct/aqueduct.dart';
+
+import 'package:server/src/internal/app_config.dart';
+
+final _config = AppConfig();
+
+ManagedContext createManagedContext() => ManagedContext(
+  ManagedDataModel([
+  //TODO: tables
+  ]),
+  PostgreSQLPersistentStore.fromConnectionInfo(
+    _config.database.username,
+    _config.database.password,
+    _config.database.host,
+    _config.database.port,
+    _config.database.databaseName,
+  ),
+);
